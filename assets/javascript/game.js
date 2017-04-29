3c430abca840b7c391f679d4a018ee24830579d5
Thomas Mercer <tmercer74@gmail.com>
2017-04-28 22:30:35 -0700

    whatever

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,45 @@
 
-function playGame ( ) {
- 
-  var answer;
-  var found = false;
-  var count = 2;
+var randomNum = getRandomNum(100);
+var guessesLeft = 9;
+var losses = 0;
+var counter = 0;
+var maxAttempts = 9;
+var guess;
+var correctGuess = false;
+//I have no idea how to complete this asssignment//
 
-  while ((count >= 0) && (found == false)) {
-    answer = Number(prompt("Guess what number I'm thinking of (It's between 1 & 100)", "")) || 0;  
-    if (answer !== ranNum) { 
-      alert("You got it wrong. You have " + count + " more chances"); 
-    }  
 
-    if (answer === ranNum) { 
-      alert("Good Guess, you must be psychic!"); 
-      found = true; 
+
+
+function getRandomNum(upper) {
+  var num = Math.floor(Math.random()*upper) + 1;
+  return num;
+}
+// while(true) {
+//   guess = prompt("I am think of a number bewteen 1 and 100. What is it?");
+//     guessesLeft += 1;
+//     if (parseInt(guess) === randomNum){
+//         correctGuess = true;
+//         break;
+//     }
+}
+document.write("You must be psychic!");
+
+while (guess != num) {
+  guess = prompt("Please pick a number between 1 and 100");
+  counter += 1;
+  console.log(counter);
+
+    if (counter > maxAttempts) {
+         alert("Sorry, you lose. the random number was" + ranNum + "Please try again!");
+              break
     }
 
-    count--;
+    if (attempts === ranNum) {
+      document.write("Congratulations! You guessed correctly!");
+      alert("The correct number was " + ranNum);
+      alert("It took you " + attempts + "tries.");
+      console.log(wins);  
+    }
   }
-  
-  if (!found) { alert("Too bad, you lose ... The number was "+ranNum); }
-  return found;
-}
 
-function generateRandomNumber() {
-  ranNum = Math.floor(Math.random()*100)+1;
-}
-
-window.onload = function() { generateRandomNumber(); }
