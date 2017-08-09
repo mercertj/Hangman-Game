e5afa638df9ef94a98cf330424a13a261841fd14
Thomas Mercer <tmercer74@gmail.com>
2017-08-09 15:49:07 -0700

    updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,81 @@
-
 var randomNum = getRandomNum(100);
-var guessesLeft = 9;
+var guessesLeft = 5;
 var losses = 0;
 var counter = 0;
-var maxAttempts = 9;
+var maxAttempts = 5;
 var guess;
 var correctGuess = false;
+var wins = 0;
 //I have no idea how to complete this asssignment//
+var wordOptions = "tanner",
+    "aaron", "margaret", "david", "leeland", "michael", "thomas", "cindy", "anna"]
+var selectedWord = "";
+var lettersinWord = [];
+var blanksAndSuccesses = [];
+var wrongLetters = [];
 
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 9;
 
+function startGame() {
+    selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+    lettersinWord = selectedWord.split("");
+    numBlanks = lettersinWord.length;
 
+    //reset
+    guessesLeft = 9;
+    wrongLetters = [];
+    blanksAndSuccesses = [];
 
-function getRandomNum(upper) {
-  var num = Math.floor(Math.random()*upper) + 1;
-  return num;
-}
-// while(true) {
-//   guess = prompt("I am think of a number bewteen 1 and 100. What is it?");
-//     guessesLeft += 1;
-//     if (parseInt(guess) === randomNum){
-//         correctGuess = true;
-//         break;
-//     }
-}
+    //populate blanks and correct guesses respectively
 
-
-while (guess != num) {
-  guess = prompt("Please pick a number between 1 and 100");
-  counter += 1;
-  console.log(counter);
-
-    if (counter > maxAttempts) {
-         alert("Sorry, you lose. the random number was" + ranNum + "Please try again!");
-              break
+    for (var i = 0; i < numBlanks; i++) {
+        blanksAndSuccesses.push("_");
     }
 
-    if (attempts === ranNum) {
-      document.write("Congratulations! You guessed correctly!");
-      alert("The correct number was " + ranNum);
-      alert("It took you " + attempts + "tries.");
-      console.log(wins);  
-    }
-  }
 
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCount;
+    document.getElementById("lossCounter").innerHTML = lossCount;
+
+
+    console.log(selectedWord);
+    console.log(lettersinWord);
+    console.log(numBlanks);
+    console.log(blanksAndSuccesses);
+}
+
+function checkLetters(letter) {
+  //check if letter exists
+    var isLetterInWord = false;
+    for (var i = 0; i < numBlanks; i++) {
+        if (selectedWord[i] == letter) {
+            isLetterInWord = true;
+        }
+    }
+    //check where letter exists in word, populate to blanksAndSucceses
+    if(isLetterInWord) {
+      for (var i=0; i<numBlanks; i++){
+        if(selectedWord[i] == letter) {
+          blanksAndSuccesses[i] = letter;
+        }
+      }
+    }
+    else {
+      wrongLetters.push(letter);
+      numGuesses++
+    }
+}
+//main process//
+
+startGame();
+
+//register keyclicks
+
+document.onkeyup = function(event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed);
+    console.log(letterGuessed);
+}
